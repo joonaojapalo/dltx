30a09dea5741982f05be1c1a875df675f81b04cb
Joona Ojapalo <joona_ojapalo@hotmail.com>
2023-01-16 15:13:35 +0200

    Add long_description to package metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md") as fd:
+    long_description = fd.read()
+
 setup(name='dltx',
       version='0.1',
       description='Direct Linear Transform (DLT)',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/joonaojapalo/dltx',
       author='Marcos Duarte',
       author_email='duartexyz@gmail.com',
